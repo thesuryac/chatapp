ae9650c9b94f6038b4aa45dfb9a20e9c85eeba41
thesuryac <thesuryac@gmail.com>
2024-03-03 09:11:54 +0530

    added to deploy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import {app, server} from './socket/socket.js'
+import path from 'path';
 
 
 import dotenv from 'dotenv';
@@ -12,7 +13,9 @@ import userRoutes from './routes/user.routes.js'
 import connectDB from './db/connectToMongoDB.js'
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,6 +23,12 @@ app.use(cookieParser());
 app.use('/api/auth',authRoutes);
 app.use("/api/message",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname,"/client/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"client","dist","index.html"))
+})
 
 server.listen(PORT,()=>{
     connectDB()
