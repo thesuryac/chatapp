6445f2270d7007c03d2913d2305db6db70eed671
thesuryac <thesuryac@gmail.com>
2024-03-05 11:21:51 +0530

    added to mobile

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import MessageContainer from '../../components/messages/MessageContainer';
 
 function Home() {
   return (
-    <div className='flex sm:h-[450px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+    <div className='flex flex-col sm:flex-row sm:h-[450px]  rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
         <SideBar/>
         <MessageContainer/>
 
